5d67ae54e5033a84945a266e8122c422744cbda3
AaronTeah <69444412+AaronTeah@users.noreply.github.com>
2024-12-14 11:39:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,7 @@ st.set_page_config(
 
 st.header("TV Show Scheduling System", divider="gray") 
 
-CO_R = st.number_input("Enter your Crossover Rate")
+CO_R = st.number_input(
+    "Enter your Crossover Rate",
+    min_value=0.00,
+    max_value=0.95)
