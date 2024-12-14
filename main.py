a3d916759b8e7f76349f3cfeead0e24ee434725d
AaronTeah <69444412+AaronTeah@users.noreply.github.com>
2024-12-14 11:41:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,10 @@ CO_R = st.number_input(
     "Enter your Crossover Rate",
     min_value=0.00,
     max_value=0.95)
+MUT_R = st.number_input(
+    "Enter your Mutation Rate", 
+    min_value=0.00,
+    max_value=0.95) 
+
+st.write("Crossover Rate: ", CO_R)
+st.write("Mutation Rate: ", MUT_R)
