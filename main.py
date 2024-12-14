be653fb8f25e3a5aea913e8bff725e9b17c10b2d
AaronTeah <69444412+AaronTeah@users.noreply.github.com>
2024-12-14 11:28:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ st.set_page_config(
   page_title="TV Show Scheduling System"
 )
 
-st.header("TV Show Scheduling System", divider="gray")
+st.header("TV Show Scheduling System", divider="gray") 
+
+x = st.slider("Crossover Rate", 0, 0.95, 25)
+st.write("x = ", x)
