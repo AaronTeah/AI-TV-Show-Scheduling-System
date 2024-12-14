ec2c25b8bce33768418b2b6b41520f2d759154f1
AaronTeah <69444412+AaronTeah@users.noreply.github.com>
2024-12-14 12:37:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import streamlit as st
 
+#streamlit header setting
+st.header("TV Show Scheduling System", divider="gray") 
 
+#customize input for crossover rate and mutation rate
+CO_R = st.number_input(
+    "Enter your Crossover Rate",
+    min_value=0.00,
+    max_value=0.95)
+MUT_R = st.number_input(
+    "Enter your Mutation Rate", 
+    min_value=0.01,
+    max_value=0.05) 
+
+st.write("Crossover Rate: ", CO_R)
+st.write("Mutation Rate: ", MUT_R) 
+#st.dataframe() 
 
 
