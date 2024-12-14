ac8ca0a5ca983ee984e0e91edb68890b8e374396
AaronTeah <69444412+AaronTeah@users.noreply.github.com>
2024-12-14 12:34:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -159,18 +159,4 @@ def genetic_algorithm(initial_schedule, generations=GEN, population_size=POP, cr
 
     return population[0]
 
-##################################################### RESULTS ###################################################################################
 
-# brute force
-initial_best_schedule = finding_best_schedule(all_possible_schedules)
-
-rem_t_slots = len(all_time_slots) - len(initial_best_schedule)
-genetic_schedule = genetic_algorithm(initial_best_schedule, generations=GEN, population_size=POP, elitism_size=EL_S)
-
-final_schedule = initial_best_schedule + genetic_schedule[:rem_t_slots]
-
-print("\nFinal Optimal Schedule:")
-for time_slot, program in enumerate(final_schedule):
-    print(f"Time Slot {all_time_slots[time_slot]:02d}:00 - Program {program}")
-
-print("Total Ratings:", fitness_function(final_schedule))
